3fb859a4008fbaaf515a9a1a172c289e7b062058
p-pawlak <peter.pawlak@rackspace.co.uk>
2016-04-26 13:51:04 +0100

    Updated FastCGI URL in test fixture

--- test/fixtures/cookbooks/rackspace_apache_php_test/attributes/fastcgi.rb
+++ test/fixtures/cookbooks/rackspace_apache_php_test/attributes/fastcgi.rb
@@ -1,1 +1,1 @@
-default['apache']['mod_fastcgi']['download_url'] = 'https://ce48de84729508ffa222-e1e4e7efebf10eb2bbd68842abc70f59.ssl.cf3.rackcdn.com/fastcgi.tar.gz'
+default['apache']['mod_fastcgi']['download_url'] = 'http://d150860c924442f0c4ba-bb788a6a02be77ad65ac0686e629b047.r71.cf3.rackcdn.com/packages/fastcgi.tar.gz'
